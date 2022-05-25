07ba644edaff89aa782e48beb56a2639dcc0162a
MinsuKim <minsu020302@gmail.com>
2022-05-25 13:55:45 +0900

    DS_8 BST main

--- src/DS_8/main.java
+++ src/DS_8/main.java
@@ -1,4 +1,17 @@
 package DS_8;
 
+
 public class main {
+    public static void main(String[] args) {
+        AVL t = new AVL();
+        t.put(60, "Banana");
+        t.put(20, "Melon");
+        t.put(10, "Orange");
+        t.put(40, "Tangerine");
+        t.put(25, "Kiwi");
+        t.put(15, "Grape");
+        t.put(80, "Strawberry");
+        t.put(70, "Cherry");
+        t.put(35, "Lemon");
+    }
 }
