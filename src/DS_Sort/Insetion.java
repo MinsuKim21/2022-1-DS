2ddd9f63ca27ba163dd877944ad19c405130144f
MinsuKim <minsu020302@gmail.com>
2022-06-01 13:43:16 +0900

    DS_sort

--- src/DS_Sort/Insetion.java
+++ src/DS_Sort/Insetion.java
@@ -1,2 +1,23 @@
-package DS_Sort;public class Insetion {
+package DS_Sort;
+
+public class Insetion {
+    public static void  sort(Comparable[] a){
+        int N =a.length;
+        for (int i=1; i<N;i++){
+            for (int j =i; j>0;j--){
+                if (isless(a[j],a[j-1]))
+                    swap(a,j,j-1);
+                else break;
+            }
+        }
+    }
+    private static void swap(Comparable[] a, int i, int j) {
+        Comparable temp = a[i];
+        a[i] = a[j];
+        a[j] = temp;
+    }
+
+    private static boolean isless(Comparable i, Comparable j) {
+        return (i.compareTo(j)<0);
+    }
 }
--- src/DS_Sort/Shell.java
+++ src/DS_Sort/Shell.java
@@ -1,2 +1,26 @@
-package DS_Sort;public class Shell {
+package DS_Sort;
+
+public class Shell {
+    public static void sort(Comparable[] a){
+        int N = a.length;
+        int h = 4;
+        while (h>=1){
+            for (int i = h; i<N;i++){
+                for (int j = i; j>=h&&isless(a[j],a[j-h]) ;j-=h) {
+                    swap(a,j,j-h);
+                }
+            }
+            h /=3;
+        }
+    }
+
+    private static void swap(Comparable[] a, int i, int j) {
+        Comparable temp = a[i];
+        a[i] = a[j];
+        a[j] = temp;
+    }
+
+    private static boolean isless(Comparable i, Comparable j) {
+        return (i.compareTo(j)<0);
+    }
 }
